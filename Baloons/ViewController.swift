2eb9f5bd1a9772431d8c1c35939a0cbf20a2712b
Home <Home@Home.local>
2016-04-18 21:30:49 +0430

    Added files for Baloons

--- Baloons/ViewController.swift
+++ Baloons/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueBaloon: UIImageView!
+    @IBOutlet weak var redBaloon: UIImageView!
+    @IBAction func blueButton(sender: UIButton) {
+        redBaloon.hidden = false
+        blueBaloon.hidden = true
+    }
+    @IBAction func redButton(sender: UIButton) {
+        redBaloon.hidden = true
+        blueBaloon.hidden = false
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
